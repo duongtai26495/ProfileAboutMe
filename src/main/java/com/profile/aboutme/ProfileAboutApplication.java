9a55e5d6aee9d4ebd58201015b7713250650e5d1
taiduong.laptop <taiduong.264@gmail.com>
2022-05-08 20:25:01 +0700

    Done source base

---
+++ src/main/java/com/profile/aboutme/ProfileAboutApplication.java
@@ -0,0 +1,13 @@
+package com.profile.aboutme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAboutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileAboutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profile/aboutme/ProfileAboutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profile.aboutme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileAboutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
